35616c69639a5cac516709f2ffe3bda5230d285d
Paul Ranshaw <p.s.ranshaw@gmail.com>
2022-03-31 14:30:22 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 token = '' #Insert Discord token here.
 prefix = ['p!'] #Prefix which Pearl will listen for preceding commands.
-cogs = ['cogs.connect', 'cogs.disconnect', 'cogs.say'] #Extensions to load upon script execution.
+cogs = ['cogs.connect', 'cogs.disconnect', 'cogs.on_message', 'cogs.say'] #Extensions to load upon script execution.
